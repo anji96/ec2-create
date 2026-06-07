071e207952958b2d5a156219c5d6281f48910560
anji96 <42612578+anji96@users.noreply.github.com>
2026-06-07 11:29:25 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,15 @@
-output "sandbox_instance_ids" {
-  value       = aws_instance.sandbox_ec2[*].id
-  description = "The unique AWS IDs assigned to all sandbox EC2 instances"
-}
-
-output "sandbox_instance_private_ips" {
-  value       = aws_instance.sandbox_ec2[*].private_ip
-  description = "The private IP addresses of all sandbox instances"
+output "sandbox_instances_summary" {
+  description = "Detailed network summary of all deployed sandbox instances"
+  value = {
+    for index, instance in aws_instance.sandbox_ec2 :
+    "instance_${index + 1}" => {
+      instance_id   = instance.id
+      subnet_id     = instance.subnet_id
+      private_ip    = instance.private_ip
+      
+      # If the instance is in a private/isolated subnet, these will gracefully return empty string ""
+      public_ip     = instance.public_ip != "" ? instance.public_ip : "N/A (No Public IP)"
+      public_dns    = instance.public_dns != "" ? instance.public_dns : "N/A (No Public DNS)"
+    }
+  }
 }
